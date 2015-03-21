6905c9ea179e952cefd3593ffc68acfa88143fe5
unknown <gabrielmoralortiz@gmail.com>
2015-03-21 16:28:07 +0100

    bitmap refactor

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,5 +1,5 @@
 class Bitmap
-    attr_reader :width, :height
+    attr_reader
 
     def self.from_source source
       data = obtain_data source
@@ -7,6 +7,38 @@ class Bitmap
       height = obtain_heigth data
       new(width, height, source)
     end
+
+    def initialize(width, height, source)
+        @data = Bitmap.obtain_data(source)
+        @width = width
+        @height = height
+    end
+
+    def wider_than? width
+      @width > width
+    end
+
+    def print connection
+      row_start = 0
+      width_in_bytes = @width / 8
+
+      while row_start < @height do
+        
+        chunk_height = chunk_height row_start
+        bytes = (0...(width_in_bytes * chunk_height)).map { @data.getbyte }
+
+        connection.write_bytes(18, 42)
+        connection.write_bytes(chunk_height, width_in_bytes)
+        connection.write_bytes(*bytes)
+        row_start += 255
+      end
+    end
+
+    private  
+
+    def chunk_height row_start
+      ((@height - row_start) > 255) ? 255 : (@height - row_start)
+    end  
 
     def self.obtain_data source
       if source.respond_to?(:getbyte)
@@ -18,55 +50,16 @@ class Bitmap
     end
 
     def self.obtain_heigth data
-      tmp = data.getbyte
-      height = (data.getbyte << 8) + tmp
-      height
+      obtain_size data
     end
 
     def self.obtain_width data
+      obtain_size data
+    end
+
+    def self.obtain_size data
       tmp = data.getbyte
-      width = (data.getbyte << 8) + tmp
-      width
-    end
-
-    def initialize(width, height, source)
-        set_source(source)
-        @width = width
-        @height = height
-    end
-
-    def wider_than? width
-      @width > width
-    end
-
-    def print connection
-      row_start = 0
-      width_in_bytes = width / 8
-      while row_start < height do
-        chunk_height = ((height - row_start) > 255) ? 255 : (height - row_start)
-        bytes = (0...(width_in_bytes * chunk_height)).map { @data.getbyte }
-
-        connection.write_bytes(18, 42)
-        connection.write_bytes(chunk_height, width_in_bytes)
-        connection.write_bytes(*bytes)
-        row_start += 255
-      end
-    end
-
-    private
-
-    def set_source(source)
-      if source.respond_to?(:getbyte)
-        @data = source
-      else
-        @data = StringIO.new(source.map(&:chr).join)
-      end
-    end
-
-    def extract_width_and_height_from_data
-      tmp = @data.getbyte
-      @width = (@data.getbyte << 8) + tmp
-      tmp = @data.getbyte
-      @height = (@data.getbyte << 8) + tmp
+      size = (data.getbyte << 8) + tmp
+      size
     end
   end
